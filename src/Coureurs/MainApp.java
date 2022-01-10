7829465396cca2c3b273cd2d4f9ed19255783711
ALAIN GIRES EBOUEL NZOM <egires@yahoo.fr>
2022-01-10 04:41:18 -0500

    4h41 9jan

--- src/Coureurs/MainApp.java
+++ src/Coureurs/MainApp.java
@@ -1,10 +1,15 @@
 package Coureurs;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
@@ -12,28 +17,64 @@ import javafx.stage.Stage;
 public class MainApp extends Application
 {
 
+	private int girlfriendCount = 3;
+	private Label labelStatut;
+	private Button addGirlFriend ;
+	private Button removeGirlsFriend ;
+	
 	@Override
 	public void start(Stage primaryStage) throws Exception
 	{
-		Label labelStatut = new Label("Hello World");
-		Rectangle rect = new Rectangle(400, 200, Color.LIGHTBLUE);
-		StackPane root = new StackPane();
-
-		root.getChildren().add(labelStatut);
-		root.getChildren().add(rect);
+		labelStatut = new Label("Number of girlfriends : 3");
 		
-		Button addGirlFriend = new Button("I have a new girlfriend");
+		VBox root = new VBox(45);
+		root.setPadding(new Insets(20));
 		
+		root.setAlignment(Pos.CENTER);
+		
+		
+		addGirlFriend = new Button("I have a new girlfriend");
+		removeGirlsFriend =  new Button("I lost a grilfriend");
 
-		rect.toBack();
+	
+		addGirlFriend.setOnAction( new EventHandler<ActionEvent>()
+		{			
+			@Override
+			public void handle(ActionEvent e)
+			{
+				girlfriendCount++;
+				UpdateGirlfriends();			   			
+			}
+		});
+		
+		removeGirlsFriend.setOnAction(new EventHandler<ActionEvent>()
+		{
+			
+			@Override
+			public void handle(ActionEvent e)
+			{
+				if(girlfriendCount > 0)
+				{
+					girlfriendCount--;
+				
+					UpdateGirlfriends();
+				}
+			}
+		});
+		
+		root.getChildren().addAll(labelStatut, addGirlFriend, removeGirlsFriend);
 
-		Scene scene = new Scene(root, 621, 500);
+		Scene scene = new Scene(root,300,400);
 
 		primaryStage.setScene(scene);
 
 		primaryStage.show();
 		primaryStage.setTitle("Coureurs De Jupons");
 
+	}
+	private void UpdateGirlfriends()
+	{
+		labelStatut.setText("New Number of girlfriend " + girlfriendCount);
 	}
 
 }
